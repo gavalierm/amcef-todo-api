b53d46f45833a49f2c5e273c689913a6dcf458ed
gavalierm <marcel@gavalier.sk>
2022-04-03 12:26:11 +0200

    User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::simplePaginate(1);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if (! $user) {
+            throw ValidationException::withMessages([
+                'user' => ['Unable to create user.'],
+            ]);
+        }
+        //event(new Registered($user)); //nepouyivam notif
+        //return $user;
+        return $this->login($request); //todo mailuer
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', Rules\Password::defaults()],
+        ]);
+ 
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        if($user->email_verified_at === null AND FALSE){ //todo mailzer
+            throw ValidationException::withMessages([
+                'email' => ['The email is not verified.'],
+            ]);
+        }
+
+        Auth::login($user);
+
+        $token = $user->tokens()->delete();
+
+        $token = $user->createToken('api')->plainTextToken;
+
+        $user['token'] = $token; //hit 
+
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user, $id)
+    {
+        //
+        //return $id;
+        return $user->findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user, int $id )
+    {
+        //
+        //return "test" . $request->user()->id;
+        //return "test" . var_dump( $id );
+        if($request->user()->id !== $id){
+            return abort(403,'You tried to update other user.');
+        }
+        //return abort(400,'Not updated');
+        $status = $user->findOrFail($id)->update($request->all());
+
+        if($status){
+            return $user->findOrFail($id);
+        }
+
+        return abort(304,'Not updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, User $user, int $id)
+    {
+        //return $id;
+        //return $request->user()->id;
+        if($request->user()->id !== $id){
+           return abort(403,'You tried to delete other user.');
+        }
+
+        $status = $user->findOrFail($id)->delete();
+
+        if($status){
+            return reponse("Removed",204);
+        }
+
+        return abort(400,"Not deleted");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('guest')->prefix('auth')->group(function(){
+    Route::post('/register', [UserController::class, 'register' ] );
+    Route::post('/login', [UserController::class, 'login' ] );
+});
+
+Route::middleware('auth:sanctum')->group(function(){
+
+    Route::get('/user', [UserController::class, 'index' ]);
+    Route::get('/user/{id}', [UserController::class, 'show' ]);
+    Route::put('/user/{id}', [UserController::class, 'update' ]);
+    Route::delete('/user/{id}', [UserController::class, 'destroy' ]);
+
+    Route::get('/register', [UserController::class, 'register' ] );
+    Route::get('/login', [UserController::class, 'login' ] );
+    Route::get('/login', [UserController::class, 'login' ] );
+
 });
